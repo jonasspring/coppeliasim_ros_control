0e04045e140e40edeec58a77701875997dedcf8d
mahmoud <mahmud.a.aly@gmail.com>
2020-07-01 06:09:07 +0200

    use transmission_interface to getjoints name/interfaces

--- include/simExtRosControl_server.h
+++ include/simExtRosControl_server.h
@@ -1,15 +1,17 @@
 #pragma once
-
 #include <ros/ros.h>
 #include <std_msgs/Int32.h>
 #include <std_msgs/String.h>
 
 // API services:
 #include "vrep_skeleton_msg_and_srv/displayText.h"
+#include <transmission_interface/transmission_parser.h>
 
-namespace MR
+
+
+namespace coppeliasim_ros_control
 {
-class MyRobot_simHW;
+class RobotSimHW;
 }
 
 namespace controller_manager
@@ -42,7 +44,7 @@ private:
 
     // Control.
     static ros::CallbackQueue * sm_rosControlCallbackQueue;
-    static MR::MyRobot_simHW * sm_myRobotHw;
+    static coppeliasim_ros_control::RobotSimHW * sm_myRobotHw;
     static controller_manager::ControllerManager * sm_ctrlManager;
     static ros::AsyncSpinner * sm_spinner;
 };
